868c7b4d4b7dd28c5a9de02919cb177fd28044a0
querencia <euxhen_hasanaj@ymail.com>
2019-02-24 15:35:42 +0200

    cmus changin next, prev behavior, adding tex image snippet

--- .config/i3/cmus.sh
+++ .config/i3/cmus.sh
@@ -19,13 +19,19 @@ if [ $1 == "play" ]; then
         fi
     fi
 elif [ $1 == "next" ]; then
-    cmus-remote -n
-    #title=`cmus-remote -C "format_print %F"`
-    #notify-send "$title"
+    stat=`cmus-remote -C status | grep status`
+    if [ "$stat" == "status playing" ]; then
+        cmus-remote -n
+        title=`cmus-remote -C "format_print %F"`
+        notify-send "$title"
+    fi
 elif [ $1 == "prev" ]; then
-    cmus-remote -r
-    #title=`cmus-remote -C "format_print %F"`
-    #notify-send "$title"
+    stat=`cmus-remote -C status | grep status`
+    if [ "$stat" == "status playing" ]; then
+        cmus-remote -r
+        title=`cmus-remote -C "format_print %F"`
+        notify-send "$title"
+    fi
 elif [ $1 == "seekf" ]; then
     cmus-remote -k +20
 elif [ $1 == "seekp" ]; then
