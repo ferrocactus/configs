0df03e315a1081624bba6294a203bc518d791eed
ferrocactus <euxhen_hasanaj@ymail.com>
2020-12-18 18:24:36 +0100

    update

--- .config/i3/cmus_playing_status.sh
+++ .config/i3/cmus_playing_status.sh
@@ -2,5 +2,5 @@
 if [ `cmus-remote -C status | sed -n 1p | grep -o paused` ]; then
     echo "Paused"
 else
-    echo `cmus-remote -C status | sed -n 2p | grep -o '[^/]*$'`
+    echo `cmus-remote -C status | sed -n 2p | grep -o '[^/]*$'` | sed 's/\&//g'
 fi
